365eb9f5c1f3ec43ffa7f2179f52947be7615254
Shawn Busolits <shawn.busolits@gmail.com>
2012-01-12 18:46:31 -0500

    Logging in, getting 404 trying to access messages

--- Narwhal Notifier/src/com/quicklookbusy/narwhalNotifier/AccountEditor.java
+++ Narwhal Notifier/src/com/quicklookbusy/narwhalNotifier/AccountEditor.java
@@ -1,7 +1,27 @@
 package com.quicklookbusy.narwhalNotifier;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -9,32 +29,105 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class AccountEditor extends Activity {
+
+	String logTag = "AccountEditor";
 	
 	EditText unameField;
-    EditText passField;
-    Button saveButton;
-    Button logoutButton;
-    TextView loginErrorLabel;
+	EditText passField;
+	Button saveButton;
+	Button logoutButton;
+	TextView loginErrorLabel;
 	
+	SharedPreferences settings;
+	Editor settingsEditor;
+
 	public class SaveListener implements OnClickListener {
 		public void onClick(View v) {
-			/*Log in
-			 *If successful, save
-			 *If not, print error message
+			/*
+			 * Log in. If successful, save. If not, print error message
 			 */
+			String uname = unameField.getText().toString();
+			String pass = passField.getText().toString();
+			String api_type = "json";
+
+			//Taken from http://www.androidsnippets.com/executing-a-http-post-request-with-httpclient
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost("https://ssl.reddit.com/api/login/" + uname);
+
+			try {
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+				nameValuePairs.add(new BasicNameValuePair("user", uname));
+				nameValuePairs.add(new BasicNameValuePair("passwd", pass));
+				nameValuePairs.add(new BasicNameValuePair("api_type", api_type));
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+				
+				HttpResponse response = httpclient.execute(httppost);
+				
+				//Taken from http://stackoverflow.com/questions/2845599/how-do-i-parse-json-from-a-java-httpresponse
+				BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+				String jsonString = reader.readLine();
+				JSONTokener tokener = new JSONTokener(jsonString);
+				JSONObject jsonResult = new JSONObject(tokener);
+				
+				Log.d(logTag, "JSON Response: " + jsonResult.toString());
+				
+				JSONObject json = jsonResult.getJSONObject("json");
+				JSONArray errors = json.getJSONArray("errors");
+				if(errors.length() > 0) {
+					loginErrorLabel.setText("Error logging you in: " + errors.getJSONArray(0).getString(1));
+					loginErrorLabel.setTextColor(Color.RED);
+					return;
+				}
+				else {
+					loginErrorLabel.setText("Success!");
+					loginErrorLabel.setTextColor(Color.GREEN);
+				}
+				JSONObject data = json.getJSONObject("data");
+				String modhash = (String) data.get("modhash");
+				String cookie = (String) data.get("cookie");
+				
+				Log.d(logTag, "JSON Cookie: " + cookie);
+				Log.d(logTag, "JSON Modhash: " + modhash);
+				
+				settingsEditor.putString("user", uname);
+				settingsEditor.putString("modhash", modhash);
+				settingsEditor.putString("cookie", cookie);	
+				settingsEditor.commit();
+
+			} catch (Exception e) {
+				Log.d(logTag, "Error sending login info: " + e.toString());
+			}
 		}
 	}
 	
+	public class LogoutListener implements OnClickListener {
+
+		public void onClick(View v) {
+			settingsEditor.clear();
+			settingsEditor.commit();
+			loginErrorLabel.setText("Logged out");
+			loginErrorLabel.setTextColor(Color.GREEN);
+		}
+		
+	}
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.account_editor);
-        
-        unameField = (EditText) findViewById(R.id.unameField);
-        passField = (EditText) findViewById(R.id.passField);
-        saveButton = (Button) findViewById(R.id.saveButton);
-        logoutButton = (Button) findViewById(R.id.logoutButton);
-        loginErrorLabel = (TextView) findViewById(R.id.loginErrorLabel);
-        
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.account_editor);
+
+		unameField = (EditText) findViewById(R.id.unameField);
+		passField = (EditText) findViewById(R.id.passField);
+		saveButton = (Button) findViewById(R.id.saveButton);
+		logoutButton = (Button) findViewById(R.id.logoutButton);
+		loginErrorLabel = (TextView) findViewById(R.id.loginErrorLabel);
+		
+		loginErrorLabel.setText("");
+		
+		saveButton.setOnClickListener(new SaveListener());
+		
+		settings = getSharedPreferences(NarwhalNotifier.PREFS_NAME, 0);
+		settingsEditor = settings.edit();
 	}
 }
--- Narwhal Notifier/src/com/quicklookbusy/narwhalNotifier/NarwhalNotifier.java
+++ Narwhal Notifier/src/com/quicklookbusy/narwhalNotifier/NarwhalNotifier.java
@@ -1,15 +1,48 @@
 package com.quicklookbusy.narwhalNotifier;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class NarwhalNotifier extends Activity {
+	
+	public static final String PREFS_NAME = "NarwhalNotifierPrefs";
+	
+	ToggleButton serviceButton;
+	Spinner frequencySpinner;
+	TextView serviceErrorLabel;
+	
+	String logTag = "NarwhalNotifier";
+	
+	Intent service;
+	
+	SharedPreferences settings;
 	
 	public class AccountEditListener implements OnClickListener {
 		
@@ -19,18 +52,88 @@ public class NarwhalNotifier extends Activity {
 		}
 	}
 	
+	public class ServiceListener implements OnClickListener {
+
+		public void onClick(View v) {
+				if(serviceButton.isChecked()) {
+					
+					if(settings.getString("user", "").equals("")) {
+						serviceErrorLabel.setText("Error: No user logged in");
+						serviceErrorLabel.setTextColor(Color.RED);
+					}
+					//service = new Intent(NarwhalNotifier.this, NarwhalNotifierService.class);
+					//startService(service);
+					
+					//Taken from http://www.androidsnippets.com/executing-a-http-post-request-with-httpclient
+					HttpClient httpclient = new DefaultHttpClient();
+					String url = "http://www.reddit.com/message/unread/.json";
+					HttpPost httppost = new HttpPost(url);
+					httppost.setHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+					
+					//String modhash = settings.getString("modhash", "");
+
+					try {
+						//List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+						//nameValuePairs.add(new BasicNameValuePair("uh", modhash));
+						//httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+						
+						HttpResponse response = httpclient.execute(httppost);
+						
+						//Taken from http://stackoverflow.com/questions/2845599/how-do-i-parse-json-from-a-java-httpresponse
+						BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+						String jsonString = reader.readLine();
+						Log.d(logTag, "First Response: " + jsonString.toString());
+						JSONTokener tokener = new JSONTokener(jsonString);
+						JSONObject jsonResult = new JSONObject(tokener);
+						
+						Log.d(logTag, "JSON Response: " + jsonResult.toString());
+					} catch (Exception e) {
+						Log.d(logTag, "Error sending login info: " + e.toString());
+					}
+				}
+				else {
+					//Kill service
+					if(settings.getBoolean("serviceRunning", false)) {
+						stopService(service);
+						serviceErrorLabel.setText("Service stopped");
+						serviceErrorLabel.setTextColor(Color.GREEN);
+					}
+					else {
+						serviceErrorLabel.setText("Error: Service not running");
+						serviceErrorLabel.setTextColor(Color.RED);	
+					}
+				}
+		}		
+	}
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
         
-        Spinner frequencySpinner = (Spinner) findViewById(R.id.frequencySpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this, R.array.planets_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        frequencySpinner.setAdapter(adapter);
+        settings = getSharedPreferences(PREFS_NAME, 0);
         
         LinearLayout accountEditTrigger = (LinearLayout) findViewById(R.id.accountEditTrigger);
         accountEditTrigger.setOnClickListener(new AccountEditListener());
+        
+        TextView subText = (TextView) findViewById(R.id.accountSubtext);
+        String user = settings.getString("user", "");
+        if(user.equals("")) {
+        	subText.setText("Not currently logged in. Click to log in.");
+        }
+        else {
+        	subText.setText("Currently logged in as " + user);
+        }
+        
+        frequencySpinner = (Spinner) findViewById(R.id.frequencySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        frequencySpinner.setAdapter(adapter);
+        
+        serviceButton = (ToggleButton) findViewById(R.id.serviceToggle);
+        serviceButton.setOnClickListener(new ServiceListener());
+        
+        serviceErrorLabel = (TextView) findViewById(R.id.serviceErrorLabel);
     }
 }
